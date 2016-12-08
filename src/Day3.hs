b086e0f67144e91f72d5b57ab1ed4edd3ce544e6
Christian Fischer <christian@chfi.se>
2016-12-08 12:00:05 +0100

    (vastly) improve day 3 solution

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,82 +2,52 @@
 
 module Main where
 
-
-import Control.Monad
-
-
-import           Data.Attoparsec.Text
-
-import Data.List (sort, transpose)
-
-import Data.Maybe (mapMaybe)
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T.IO
-
-import System.Environment (getArgs)
+import           Control.Monad
+import           Data.List            (sort, transpose)
+import           Data.Attoparsec.Text hiding (take)
+import           Data.Maybe           (mapMaybe)
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import qualified Data.Text.IO         as T.IO
+import           System.Environment   (getArgs)
 
 newtype Tri = Tri (Int, Int, Int) deriving (Eq, Show)
 
-newTri :: [Int] -> Maybe Tri
-newTri e@[_,_,_] = Just $ Tri (s,m,l)
+tri :: (Int, Int, Int) -> Tri
+tri (a,b,c) = Tri (s,m,l)
+  where [s,m,l] = sort [a,b,c]
+
+tryTri :: [Int] -> Maybe Tri
+tryTri e@[_,_,_] = Just $ Tri (s,m,l)
   where [s,m,l] = sort e
-newTri _ = Nothing
+tryTri _ = Nothing
 
 isLegal :: Tri -> Bool
 isLegal (Tri (s,m,l)) = (s + m) > l
 
-parseTri :: Text -> Maybe Tri
-parseTri t = newTri $ fmap (read . T.unpack) $ T.words t
+parseInput :: Parser [Tri]
+parseInput = many' (parseTri <* endOfLine) <* endOfInput
 
-limList :: [a] -> [[a]]
-limList [] = []
-limList ts = h : limList t
-  where (h, t) = splitAt 3 ts
+parseTri :: Parser Tri
+parseTri = do
+  let piece = skipSpace >> decimal
+      line = (,,) <$> piece <*> piece <*> piece <* endOfLine
+  tri <$> line
 
-parseWords :: Text -> [[Text]]
-parseWords t = fmap T.words $ T.lines t
-
-transposeInput :: Text -> Text
-transposeInput t = T.unlines $ fmap T.unwords $ transpose $ fmap T.words $ T.lines t
-
-parseInput :: Text -> [Tri]
-parseInput t = mapMaybe parseTri $ T.lines t
-
-parseInput' :: Parser [Tri]
-parseInput' = many' (parseTri' <* endOfLine) <* endOfInput
-
-
-parseTri' :: Parser Tri
-parseTri' = do
-  skipSpace
-  a <- decimal
-  skipSpace
-  b <- decimal
-  skipSpace
-  c <- decimal
-  let [s,m,l] = sort [a,b,c]
-  return $ Tri (s,m,l)
-
-
+parseTris :: Parser [Tri]
+parseTris = do
+  let piece = skipSpace >> decimal
+      line = count 3 piece <* endOfLine
+  mapMaybe tryTri <$> transpose <$> count 3 line
 
 main :: IO ()
 main = do
   contents <- T.IO.readFile =<< head <$> getArgs
 
-  case parseOnly parseInput' contents of
+  case parseOnly (many' parseTri) contents of
     Left e -> print e
-    Right ts -> print ts
+    Right ts -> print $ length $ filter isLegal ts
 
-  -- let tris' = case parseInput' contents
-  --       Done _ ts -> Just ts
-  --       _ -> Nothing
-
-  -- let t'd = fmap T.words $ T.lines $ transposeInput $ contents
-  --     t'd2 = fmap limList t'd
-  --     tris = (concatMap . mapMaybe) (parseTri . T.unwords) t'd2
-
-  -- print t'd
-  -- print t'd2
-  -- print tris
-  -- print $ length $ filter isLegal tris
+  case parseOnly (many' parseTris) contents of
+    Left e -> print e
+    Right ts -> print $ length $ filter isLegal $ concat ts
